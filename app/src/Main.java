8dc1fe1b57d061314c58321b70b85823ff48d02d
Max Matveenko <supmaks8653@gmail.com>
2025-07-01 00:25:47 +0300

    Test change

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,21 +1,25 @@
 package src;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         int port = 9090;
-        ServerSocket server = new ServerSocket(port);
-        System.out.println("Listening on port " + port);
-        while (true) {
-            try (Socket client = server.accept()) {
-                OutputStream out = client.getOutputStream();
-                String response = "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nok";
-                out.write(response.getBytes());
-                out.flush();
+        try(ServerSocket server = new ServerSocket(port)) {
+            System.out.println("Listening on port " + port);
+            while (true) {
+                try (Socket client = server.accept()) {
+                    OutputStream out = client.getOutputStream();
+                    String response = "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nok";
+                    out.write(response.getBytes());
+                    out.flush();
+                }
             }
+        } catch (IOException exception){
+            exception.printStackTrace();
         }
     }
 }
